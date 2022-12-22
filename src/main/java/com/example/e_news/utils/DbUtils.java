d7678b438f2d516096db77c86dbba9c8417aefe2
HaiDangFIT <dangtqh.fit@gmail.com>
2022-12-23 04:22:37 +0700

    check exist name

--- src/main/java/com/example/e_news/utils/DbUtils.java
+++ src/main/java/com/example/e_news/utils/DbUtils.java
@@ -5,7 +5,7 @@ import org.sql2o.Sql2o;
 
 public class DbUtils {
     //static Sql2o sql2o = new Sql2o("jdbc:mysql://localhost:3306/enews", "root", "");
-    static Sql2o sql2o = new Sql2o("jdbc:mysql://localhost:3306/e", "root", "haidang28@G");
+    static Sql2o sql2o = new Sql2o("jdbc:mysql://localhost:3306/e", "root", "");
 
     public static Connection getConnection(){
         return sql2o.open();
